0ddbb2733d6584ba4b4ee6b7486332408468e819
giusepper <giuseppe.rotolo@venturedevs.com>
2018-10-12 11:46:23 +0200

    Added reminder for debugging with terminal

--- playground/debugging.js
+++ playground/debugging.js
@@ -7,4 +7,16 @@ person.name = 'Giuseppe';
 
 debugger; //breakpoint
 
+// to start node in debug:
+// node inspect <filename>
+// in debug mode:
+// n -> next line
+// c -> next breakpoint or go to the end
+// repl -> go the the variable inspector
+// in repl mode:
+// print anything just using variable name
+// to exit:
+// control + C
+
+
 person.age = 28;
